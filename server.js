f19780713fc23e39c43287546db7d316cbc67e96
Dennis Onder <webdev.dennisonder@gmail.com>
2018-11-15 02:13:51 +0100

    y u no work

--- server.js
+++ server.js
@@ -3,17 +3,18 @@ const client = require('socket.io').listen(9000).sockets;
 
 // Connect to MongoDB
 const mongoURI = 'mongodb://root:root123@ds039351.mlab.com:39351/socket_io-crash-course';
-mongo.connect(mongoURI, { useNewUrlParser: true }, (err, db) => {
+mongo.connect(mongoURI, { useNewUrlParser: true }, (err, client) => {
   if(err) {
     throw err;
+  } else {    
+    console.log('Database connected');
   }
-  console.log('Database connected');
   // Connect to Socket.io
   client.on('connection', (socket) => {
-    let chat = db.collection('chats');
+    let chat = client.db.collection('chats');
     // Create function to send status
-    sendStatus = function(s) {
-      socket.emit('Status', s)
+    const sendStatus = function(s) {
+      socket.emit('status', s)
     }
     // Get chats from collection
     chat.find().limit(100).sort({ _id: 1 }).toArray(function(err, res) {
@@ -21,7 +22,7 @@ mongo.connect(mongoURI, { useNewUrlParser: true }, (err, db) => {
         throw err;
       }
       // Emit the messages
-      socket.emit('Output', res);
+      socket.emit('output', res);
     });
     // Handle input events
     socket.on('input', (data) => {
@@ -34,7 +35,7 @@ mongo.connect(mongoURI, { useNewUrlParser: true }, (err, db) => {
       } else {
         // Insert message to database
         chat.insert({name: name, message: message}, () => {
-          client.emit('Output', [data]);
+          client.emit('output', [data]);
           // Send status object
           sendStatus({message: 'Message sent.', clear: true});
         })
@@ -44,7 +45,7 @@ mongo.connect(mongoURI, { useNewUrlParser: true }, (err, db) => {
       // Remove all chats from collection
       chat.remove({}, () => {
         // Emit cleared
-        socket.emit('Cleared');
+        socket.emit('cleared');
       })
     })
   });
